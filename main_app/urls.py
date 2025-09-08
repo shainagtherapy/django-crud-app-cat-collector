f8ee474088589c4ea928fa8015b24523551a24d9
Shaina G <shainagtherapy@gmail.com>
2025-09-08 12:07:46 -0700

    completed three templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cats-index'),
 ]
